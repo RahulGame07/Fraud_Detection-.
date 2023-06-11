f39b243533b1833074fd1fa0113dd7e8a51d508f
RahulGame07 <rahulgame982@gmail.com>
2023-06-11 16:26:26 +0530

    model2 is final model

--- ml_model.py
+++ ml_model.py
@@ -3,9 +3,11 @@ print("Entire code for ML model")
 def mdoel1():
     print("adding code for ML models1 ")
 
-def model2()
+def model2():
     print('adding code for ML models2')
 
 
-def model3()
+def model3():
     print('adding code for ML models3')
+
+print('model 2 is performing best')
