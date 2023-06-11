b84545cd9dbca4290ee265a76afd9fac1c63fbf6
RahulGame07 <rahulgame982@gmail.com>
2023-06-11 16:24:14 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("Entire code for ML model")
+
+def mdoel1():
+    print("adding code for ML models1 ")
+
+def model2()
+    print('adding code for ML models2')
+
+
+def model3()
+    print('adding code for ML models3')
